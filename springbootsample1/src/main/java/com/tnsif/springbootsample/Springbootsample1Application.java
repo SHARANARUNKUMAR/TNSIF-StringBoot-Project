db0340683a1c29cebbfaff6653f9afaf830af2b9
Sharan <Sharan@Dell>
2025-08-09 14:25:01 +0530

    Demo Project

---
+++ springbootsample1/src/main/java/com/tnsif/springbootsample/Springbootsample1Application.java
@@ -0,0 +1,13 @@
+package com.tnsif.springbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootsample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootsample1Application.class, args);
+	}
+
+}
